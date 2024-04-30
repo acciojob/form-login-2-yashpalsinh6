47d059924c88db6728c3616f23bd16f28e4cec01
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-30 16:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function displayFormValues(){ 
+	var name=document.getElementById('fname').value;
+	var last=document.getElementById('lname').value;
+	var phone=document.getElementById('Pnum').value;
+	var email=document.getElementById('eid').value;
+
+	alert('First Name:'+name+'\nLast Name:'+last+'\nPhone Number:'+phone+'\nEmail ID:'+email);
+}
